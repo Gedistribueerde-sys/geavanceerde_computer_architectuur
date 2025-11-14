bba0e25c379ce08a4fbafb90d9a25a466472cb61
thibaut-beck <thibaut.beckske@gmail.com>
2025-11-14 10:36:19 +0100

    filter is also working for a .las file in python

--- project/pycode/CODE/pointCloudVisualizer.py
+++ project/pycode/CODE/pointCloudVisualizer.py
@@ -2,7 +2,9 @@ import numpy as np
 import open3d as o3d
 import laspy as lp
 
-pcd_path = "project/pycode/DATA/neighborhood.las"
+pcd_path = "../DATA/neighborhood.las"
+
+VOXEL_SIZE=0.5
 
 # Import dataset with laspy
 point_cloud = lp.read(pcd_path)
@@ -16,3 +18,38 @@ pcd.colors = o3d.utility.Vector3dVector(rgb)
 
 # Visualize the point cloud
 o3d.visualization.draw_geometries([pcd])
+
+# getting the minimas
+mins = np.min(xyz, axis=0)
+maxs = np.max(xyz, axis=0)
+
+#normalise this
+print("some basic printouts ")
+print (mins)
+print("-------")
+print (maxs)
+xyz_shifted = xyz - mins
+
+
+voxel_indices = (xyz_shifted // VOXEL_SIZE).astype(int)
+
+snapped_points = (voxel_indices * VOXEL_SIZE) + (VOXEL_SIZE / 2)
+
+# Visualize Snapped Points as Voxel Blocks
+# Create a mesh for each voxel
+mesh_list = []
+for i, (center, color) in enumerate(zip(snapped_points, rgb)):
+    # Create a cube centered at the voxel center
+    cube = o3d.geometry.TriangleMesh.create_box(width=VOXEL_SIZE, height=VOXEL_SIZE, depth=VOXEL_SIZE)
+    # Translate cube to voxel center
+    cube.translate(center - VOXEL_SIZE / 2)
+    # Assign color to the cube
+    cube.paint_uniform_color(color)
+    mesh_list.append(cube)
+
+# Combine all meshes
+voxel_mesh = o3d.geometry.TriangleMesh()
+for mesh in mesh_list:
+    voxel_mesh += mesh
+
+o3d.visualization.draw_geometries([voxel_mesh], window_name="Voxelized Blocks")
